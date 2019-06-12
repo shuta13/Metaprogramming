2ee082e777720a68d35dbbb706da22ed51c352a2
shuta13 <hollystarsun@gmail.com>
2019-06-12 11:35:13 +0900

    updated

--- ExcludedMeta/flat_scope.rb
+++ ExcludedMeta/flat_scope.rb
@@ -1,12 +1,12 @@
-shared = 'shared'
+flat = 'flat'
 
 # クラスをメソッドとして呼び出してる
 MyClass = Class.new do
-  p "クラス定義の中は#{shared}"
+  p "クラス定義の中は#{flat}"
 
   # 動的なメソッドの生成
   define_method :my_method do 
-    "メソッド定義の中も#{shared}"
+    "メソッド定義の中も#{flat}"
   end
 end
 
